b15c30529ff7702e5f9def0d906f69be128198c7
Mary-Orlova <vampusha07@gmail.com>
2023-11-20 16:02:44 +0300

    add comments

--- main.py
+++ main.py
@@ -2,12 +2,18 @@ from collections import Counter
 
 
 def get_simbol():
+    """Метод нахождения символов в файловом документе"""
     try:
+        """Блок нахождения файла, открытия и анализа поиска символов"""
         with open('text.txt', 'r', encoding="utf-8") as text_file:
+            # "в переменную read помещаем открытый файл чтения, убрав из него все пробелы и переносы строк"
             read = text_file.read().lower().replace(' ', '').replace('\n', '')
+            # "все символы равняются длине полученного открытого файла чтения "
             all_simbols = len(read)
-            c = Counter(read)
-            res_freq = {tup[0]: (str(round(int(tup[1])*100/all_simbols, 2))+'%') for tup in c.most_common()}
+            # "переменная с - счетчик для частоты каждого символа в файле"
+            count = Counter(read)
+            # "переменная res_freq - словарь из ключа-символа и значения-количество частоты в файле"
+            res_freq = {tup[0]: (str(round(int(tup[1])*100/all_simbols, 2))+'%') for tup in count.most_common()}
 
         # print('Общее количество символов в файле: ', all_simbols)
 
@@ -15,6 +21,7 @@ def get_simbol():
             print("{0}: {1}".format(key, value))
 
     except FileNotFoundError:
+        """Блок ошибки нахождения файла"""
         print('файл не найден')
 
 
